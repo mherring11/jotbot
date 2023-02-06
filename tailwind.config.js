a80e7773dfa17bddd43ef1344e79c510e7e09138
Michael Herring <m.herring11@yahoo.com>
2023-02-05 21:43:12 -0600

    fixed opening sentence

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
-const plugin = require('tailwindcss/plugin')
-
 module.exports = {
+  content: [],
   theme: {
-    extend: {
-      textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
-    },
+    extend: {},
   },
   plugins: [
-    plugin(function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'text-shadow': (value) => ({
-            textShadow: value,
-          }),
-        },
-        { values: theme('textShadow') }
-      )
-    }),
+    require('tailwindcss-textshadow')
   ],
 }
